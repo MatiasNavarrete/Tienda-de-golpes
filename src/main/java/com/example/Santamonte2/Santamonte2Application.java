937c110196fb287bf71f2938397bb3b796cc9469
Your Name <mat.navarreted@duocuc.cl>
2026-04-16 16:39:53 -0400

    El inicio de la tortura

---
+++ src/main/java/com/example/Santamonte2/Santamonte2Application.java
@@ -0,0 +1,13 @@
+package com.example.Santamonte2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Santamonte2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Santamonte2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Santamonte2/Santamonte2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Santamonte2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Santamonte2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
