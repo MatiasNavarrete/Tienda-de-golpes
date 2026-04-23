4c3cce63d3f7fdb6add5f0009142c8b626243ec9
josem <jm.romero@duocuc.cl>
2026-04-23 16:57:09 -0400

    feat: Entidad producto

--- producto/producto/src/main/java/com/example/producto/model/Producto.java
+++ producto/producto/src/main/java/com/example/producto/model/Producto.java
@@ -1,4 +1,28 @@
 package com.example.producto.model;
 
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
 public class Producto {
+
+    @NotNull
+    private int id;
+    @NotBlank
+    private String nombre;
+    @NotBlank
+    private String descripción;
+    @NotNull
+    private int precio;
+    @NotNull
+    private int stock;
+
 }
