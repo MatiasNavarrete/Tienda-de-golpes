cefd82395ab0a35cc49ffa840a5307ea05ebb9ee
josem <jm.romero@duocuc.cl>
2026-04-23 17:13:10 -0400

    refactor: Uso de anotaciones @Entity, @Size, @Positive, @Id y @GeneratedValue.

--- producto/producto/src/main/java/com/example/producto/model/Producto.java
+++ producto/producto/src/main/java/com/example/producto/model/Producto.java
@@ -1,28 +1,36 @@
 package com.example.producto.model;
 
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.NotBlank;
 
+
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 
-
 public class Producto {
-
-    @NotNull
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @NotBlank
+
+    @NotBlank(message = "El nombre del producto es obligatorio")
+    @Size(min= 3, max= 50, message = "El nombre del producto debe tener entre 3 y 50 caracteres")
     private String nombre;
-    @NotBlank
+
+    @NotBlank(message = "La descripción del producto no debe estar vacía")
     private String descripción;
-    @NotNull
+
+    @Positive(message = "El precio debe ser mayor a cero")
     private int precio;
-    @NotNull
+
+    @Min(value = 0, message = "El stock no puede se menor a cero")
     private int stock;
 
 }
