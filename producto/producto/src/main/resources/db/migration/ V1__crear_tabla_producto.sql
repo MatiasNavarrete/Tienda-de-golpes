d493340dbb72800b13ebba2c09345ed416e8410e
Joses44 <jm.romero@duocuc.cl>
2026-04-27 18:00:10 -0400

    feat: añadidos datos que faltaban en el CREATE TABLE producto().

--- producto/producto/src/main/resources/db/migration/ V1__crear_tabla_producto.sql
+++ producto/producto/src/main/resources/db/migration/ V1__crear_tabla_producto.sql
@@ -1,5 +1,7 @@
 CREATE TABLE producto(
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(100),
-    precio INT
+    nombre VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(200),
+    precio INT,
+    stock INT NOT NULL
 );
