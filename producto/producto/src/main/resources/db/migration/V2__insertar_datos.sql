7c6407141511528aaeb76d97fe525144b59a6057
josem <jm.romero@duocuc.cl>
2026-04-30 18:41:34 -0400

    refactor: V2__insertar_datos.sql. faltaban los datos de stock para los 3 productos iniciales, lo que hacía que no corriera el proyecto.

--- producto/producto/src/main/resources/db/migration/V2__insertar_datos.sql
+++ producto/producto/src/main/resources/db/migration/V2__insertar_datos.sql
@@ -1,3 +1,8 @@
-INSERT INTO producto(nombre, precio) VALUES ('Combo en la guata', 99);
-INSERT INTO producto(nombre, precio) VALUES ('Combo en el brazo', 50);
-INSERT INTO producto(nombre, precio) VALUES ('Combo en los c*c*s', 1000);
+INSERT INTO producto (nombre, descripcion, precio, stock)
+VALUES ('Combo en la guata', 'Un puñetazo directo al abdomen, es posible que quedes sin aire.', 99, 10);
+
+INSERT INTO producto (nombre, descripcion, precio, stock)
+VALUES ('Combo en el brazo', 'Puede dejarte el brazo con un moretón, no recomendado si planeas usar el brazo dentro de los siguientes 4 días.', 50, 25);
+
+INSERT INTO producto (nombre, descripcion, precio, stock)
+VALUES ('Combo en los testículos','El producto estrella de la tienda, altamente efectivo y doloroso, podrías quedar sin descendencia.', 1000, 5);
