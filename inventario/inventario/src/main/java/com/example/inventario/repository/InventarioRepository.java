5629d2cf94561c900060a0da20d96d1f530e32fd
josem <jm.romero@duocuc.cl>
2026-04-30 20:29:01 -0400

    refactor(inventario): se mueve el InventarioRepository a su correspondiente carpeta, anteriormente estaba en carpeta model.

--- inventario/inventario/src/main/java/com/example/inventario/repository/InventarioRepository.java
+++ inventario/inventario/src/main/java/com/example/inventario/repository/InventarioRepository.java
@@ -1,4 +1,4 @@
-package com.example.inventario;
+package com.example.inventario.repository;
 
 import com.example.inventario.model.Inventario;
 import org.springframework.data.jpa.repository.JpaRepository;
