9e6fd925fa2dcb520609580fe3d01bd7aef2c622
josem <jm.romero@duocuc.cl>
2026-04-30 20:26:42 -0400

    feat(inventario): Implementa el InventarioRepository.

---
+++ inventario/inventario/src/main/java/com/example/inventario/InventarioRepository.java
@@ -0,0 +1,15 @@
+package com.example.inventario;
+
+import com.example.inventario.model.Inventario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface InventarioRepository extends JpaRepository<Inventario, Long> {
+
+    //método para buscar el stock mediante el ID del producto.
+    Optional<Inventario> findByProductoId(Long productoId);
+
+}
