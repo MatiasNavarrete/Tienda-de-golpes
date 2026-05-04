09a48ed10a139e96387e948b70ad31a601998999
josem <jm.romero@duocuc.cl>
2026-05-04 17:33:32 -0400

    feat(carrito): añada entidad de Carrito.

--- carrito/carrito/src/main/java/com/example/carrito/model/Carrito.java
+++ carrito/carrito/src/main/java/com/example/carrito/model/Carrito.java
@@ -1,7 +1,25 @@
 package com.example.carrito.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "carrito")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Carrito {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "usuario_id", nullable = false)
+    private String usuarioId;
+
+    @OneToMany(mappedBy = "carrito", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ItemCarrito> items;
+
 }
