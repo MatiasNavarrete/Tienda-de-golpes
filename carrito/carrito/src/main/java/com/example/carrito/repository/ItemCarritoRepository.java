5e311538e343d147707cb69e902684d6fb7a4858
josem <jm.romero@duocuc.cl>
2026-05-05 16:56:13 -0400

    feat(carrito): añade dos métodos nuevos en ItemCarritoRepository.

--- carrito/carrito/src/main/java/com/example/carrito/repository/ItemCarritoRepository.java
+++ carrito/carrito/src/main/java/com/example/carrito/repository/ItemCarritoRepository.java
@@ -1,10 +1,20 @@
 package com.example.carrito.repository;
 
 import com.example.carrito.model.ItemCarrito;
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ItemCarritoRepository extends JpaRepository<ItemCarrito, Long> {
+
+    //buscar items de un carrito específico
+    List<ItemCarrito> findByCarritoId(Long carritoId);
+
+    //buscar un producto específico dentro de un carrito
+    Optional<ItemCarrito> findbyCarritoIdAndProductoID(Long carritoId, Long productoId);
 
 }
