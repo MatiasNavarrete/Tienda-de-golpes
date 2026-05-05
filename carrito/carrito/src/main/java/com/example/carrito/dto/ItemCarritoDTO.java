df10b2d0efa2c8d70c382c4a177ac30eb6481f52
josem <jm.romero@duocuc.cl>
2026-05-05 16:42:29 -0400

    feat(carrito) implementa ItemCarritoDTO.

---
+++ carrito/carrito/src/main/java/com/example/carrito/dto/ItemCarritoDTO.java
@@ -0,0 +1,16 @@
+package com.example.carrito.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemCarritoDTO {
+    private Long productoId;
+    private String nombre;
+    private Double precio;
+    private Integer cantidad;
+
+}
