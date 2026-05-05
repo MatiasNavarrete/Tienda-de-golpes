22c8ba39c015ad2f2650a780125af7d40ef59309
josem <jm.romero@duocuc.cl>
2026-05-05 17:44:51 -0400

    feat(carrito) implementa CarritoController.

--- carrito/carrito/src/main/java/com/example/carrito/controller/CarritoController.java
+++ carrito/carrito/src/main/java/com/example/carrito/controller/CarritoController.java
@@ -1,4 +1,36 @@
 package com.example.carrito.controller;
 
+import com.example.carrito.dto.CarritoDTO;
+import com.example.carrito.dto.ItemRequestDTO;
+import com.example.carrito.service.CarritoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/carrito")
 public class CarritoController {
+
+    @Autowired
+    private CarritoService carritoService;
+
+    //agregar producto al carrito
+    @PostMapping("/{usuarioId}")
+    public ResponseEntity<CarritoDTO> agregarProducto(
+            @PathVariable String usuarioId,
+            @Valid @RequestBody ItemRequestDTO request) {
+
+        CarritoDTO carrito = carritoService.agregarProducto(usuarioId, request);
+        return ResponseEntity.ok(carrito);
+
+    }
+
+    @GetMapping("/{usuarioId}")
+    public ResponseEntity<CarritoDTO> obtenerCarrito(@PathVariable String usuarioId) {
+        CarritoDTO carrito = carritoService.obtenerCarrito(usuarioId);
+        return ResponseEntity.ok(carrito);
+
+    }
+
 }
