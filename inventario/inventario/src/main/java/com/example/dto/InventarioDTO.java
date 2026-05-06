75ef55d89de9a3726b87403d392b43ca4b066860
josem <jm.romero@duocuc.cl>
2026-05-06 19:25:46 -0400

    feat(inventario): implementa InventarioDTO con etiquetas de validación.

---
+++ inventario/inventario/src/main/java/com/example/dto/InventarioDTO.java
@@ -0,0 +1,18 @@
+package com.example.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+
+@Data
+public class InventarioDTO {
+
+    private Long id;
+
+    @NotNull(message = "¡El ID del producto es obligatorio!")
+    private Long productoId;
+
+    @PositiveOrZero(message = "¡El stock no puede ser negativo!")
+    private int stock;
+
+}
