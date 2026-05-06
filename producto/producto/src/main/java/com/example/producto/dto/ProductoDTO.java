592eff0f02546a1676cdc87b60a6809e4b6b48ab
josem <jm.romero@duocuc.cl>
2026-05-06 18:20:44 -0400

    feat(productos): añade etiquetas de validaciones en ProductoDTO.

--- producto/producto/src/main/java/com/example/producto/dto/ProductoDTO.java
+++ producto/producto/src/main/java/com/example/producto/dto/ProductoDTO.java
@@ -1,5 +1,8 @@
 package com.example.producto.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +12,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ProductoDTO {
     private Long id;
+
+    @NotBlank(message = "¡El nombre del producto es obligatorio!")
     private String nombre;
+
+    @NotBlank(message = "¡La descripción del producto es obligatoria!")
     private String descripcion;
+
+    @Positive(message = "¡El precio del producto debe ser mayor a 0!")
     private int precio;
+
+    @Min(value = 0, message = "¡El stock del producto no puede ser negativo!")
     private Integer stock; //campo nuevo que viene del otro servicio inventario
 }
