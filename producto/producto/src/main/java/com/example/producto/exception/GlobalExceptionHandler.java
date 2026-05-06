d97984c497899559a1a1f824a02868332f6686d1
josem <jm.romero@duocuc.cl>
2026-05-06 19:07:20 -0400

    feat(producto): añade GlobalExceptionHandler para las validaciones.

---
+++ producto/producto/src/main/java/com/example/producto/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.example.producto.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        //recorre los errores de validación encontrados y los pone en un mapa
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return errors;
+    }
+}
