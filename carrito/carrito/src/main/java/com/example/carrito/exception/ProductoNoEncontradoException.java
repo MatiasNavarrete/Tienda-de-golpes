cf979ccfa29a514da93e302f62f79e8aa2979bf2
josem <jm.romero@duocuc.cl>
2026-05-07 17:45:45 -0400

    feat(carrito): elimina importación no usada en ProductoNoEncontradoException.

--- carrito/carrito/src/main/java/com/example/carrito/exception/ProductoNoEncontradoException.java
+++ carrito/carrito/src/main/java/com/example/carrito/exception/ProductoNoEncontradoException.java
@@ -1,6 +1,5 @@
 package com.example.carrito.exception;
 
-import com.example.carrito.dto.ProductoResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
