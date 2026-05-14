bccb0f60a98f5cc052948693010a05455b326ee2
Your Name <mat.navarreted@duocuc.cl>
2026-05-14 18:05:45 -0400

    feat: se logro correr la aplicacion con el logback y se agrego el modelo de usuario y el repositorio de este mismo

---
+++ usuarios/src/main/java/com/example/usuarios/model/Usuario.java
@@ -0,0 +1,19 @@
+package com.example.usuarios.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String email;
+    private String password;
+
+}
