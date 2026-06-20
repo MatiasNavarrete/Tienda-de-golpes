1e55a80976f98b23e5dfb683ef5232c862ce975a
Your Name <mat.navarreted@duocuc.cl>
2026-06-20 19:53:31 -0400

    feat: implementacion de swagger y hateos para notificacion

--- notificaciones/src/main/java/com/example/notificaciones/Controller/NotificacionController.java
+++ notificaciones/src/main/java/com/example/notificaciones/Controller/NotificacionController.java
@@ -1,22 +1,34 @@
 package com.example.notificaciones.Controller;
 
+
 import com.example.notificaciones.dto.NotificacionDto;
+import com.example.notificaciones.model.Notificacion;
 import com.example.notificaciones.service.NotificacionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/notificaciones")
+@Tag(name = "Notificaciones", description = "Endpoints para el procesamiento de alertas del ecosistema")
 public class NotificacionController {
 
     @Autowired
-    private NotificacionService service;
+    private NotificacionService notificacionService;
 
     @PostMapping
-    public void recibirNotificacion(@RequestBody NotificacionDto dto) {
-        service.procesarNotificacion(dto);
+    @Operation(summary = "Recibir y procesar una notificación", description = "Recibe el DTO desde otros microservicios, lo guarda en la DB y genera enlaces HATEOAS")
+    public ResponseEntity<EntityModel<NotificacionDto>> recibirNotificacion(@RequestBody NotificacionDto dto) {
+        notificacionService.procesarNotificacion(dto);
+
+        EntityModel<NotificacionDto> modelo = EntityModel.of(dto);
+
+        modelo.add(linkTo(methodOn(NotificacionController.class).recibirNotificacion(dto)).withSelfRel());
+
+        return ResponseEntity.ok(modelo);
     }
 }
--- notificaciones/src/main/java/com/example/notificaciones/NotificacionesApplication.java
+++ notificaciones/src/main/java/com/example/notificaciones/NotificacionesApplication.java
@@ -3,7 +3,9 @@ package com.example.notificaciones;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springdoc.core.configuration.SpringDocHateoasConfiguration.class
+})
 public class NotificacionesApplication {
 
 	public static void main(String[] args) {
